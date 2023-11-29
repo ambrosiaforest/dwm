400a52e260fac1f4fc2c225c6760a33b1e348b90
mrawesomeplus <kj@vnetwork.com>
2023-11-28 19:58:30 -0600

    updated dwm

--- config.h
+++ config.h
@@ -1,15 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 10;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "undefined-medium:size=13" };
-static const char dmenufont[]       = "undefined-medium:size=13";
+static const int vertpadbar         = 0;        /* vertical padding for statusbar */
+static const char *fonts[]          = { "undefined-medium:size=14" };
+static const char dmenufont[]       = "undefined-medium:size=14";
 static unsigned int baralpha		= 0xd0;
 static unsigned int borderalpha		= OPAQUE;
 static const char col_gray1[]       = "#222222";
@@ -35,6 +35,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,        0,            1,           -1 },
+    { "icecat",  NULL,       "Picture-in-Picture",         0,       1,      -1 },
 	{ NULL,      NULL,     "Event Tester", 0,            0,           -1 },
 	
 };
@@ -68,20 +69,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *scrotcmd[] = { "/bin/sh", "-c", "scrot -f '/tmp/clipboard.png' -o -s -e 'xclip -selection clipboard -t image/png -i $f'", NULL};
-static const char *volupcmd[] = { "wpctl", "set-volume", "@DEFAULT_SINK@", "5%+", NULL };
-static const char *voldowncmd[] = { "wpctl", "set-volume", "@DEFAULT_SINK@","5%-", NULL };
-static const char *volmutecmd[] = { "wpctl", "set-mute", "@DEFAULT_SINK@", "toggle", NULL };
 
-#include <X11/XF86keysym.h>
 static const Key keys[] = {
 	/* modifier                     key         function            argument */
 	{ MODKEY,                       XK_d,       spawn,              {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return,  spawn,              {.v = termcmd } },
-    { 0,                            XK_Print,   spawn,              {.v = scrotcmd } },
-    { MODKEY,                       XK_F8,      spawn,              {.v = volmutecmd } },
-    { MODKEY,                       XK_F9,      spawn,              {.v = voldowncmd } },
-    { MODKEY,                       XK_F10,     spawn,              {.v = volupcmd } },
+    { 0,                            XK_Print,   spawn,              SHCMD("maim -s | xclip -selection clipboard -t image/png")},
 	{ MODKEY,                       XK_b,       togglebar,          {0} },
 	{ MODKEY,                       XK_j,       focusstack,         {.i = +1 } },
 	{ MODKEY,                       XK_k,       focusstack,         {.i = -1 } },
@@ -107,6 +100,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_equal,   setgaps,            {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,   setgaps,            {.i = 0  } },
     { MODKEY|ShiftMask,             XK_f,       togglefullscr,      {0} },
+    { MODKEY,                       XK_s,       togglesticky,       {0} },
 	TAGKEYS(                        XK_1,                           0)
 	TAGKEYS(                        XK_2,                           1)
 	TAGKEYS(                        XK_3,                           2)
@@ -117,9 +111,6 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                           7)
 	TAGKEYS(                        XK_9,                           8)
 	{ MODKEY|ShiftMask,             XK_q,	    quit,		        {0} },
-    { 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
-    { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
-    { 0,                            XF86XK_AudioMute, spawn,        {.v = volmutecmd } },
 };
 
 
